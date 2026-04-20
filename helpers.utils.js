35e97af16ab88c282a1196ddd6b55004ad749589
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-04-21 00:27:23 +0530

    perf: optimize function

---
+++ helpers.utils.js
@@ -0,0 +1,10 @@
+
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString("en-IN");
+}
+
+export function slugify(text) {
+  return text.toLowerCase().replace(/\s+/g, "-");
+}
+
+
